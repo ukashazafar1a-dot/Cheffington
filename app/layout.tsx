6623c8fc524361e6360d733907803c1a653bed51
umer <umern7833@gmail.com>
2026-05-16 14:14:14 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="">
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="" suppressHydrationWarning>
         <Navebar />
         {children}
         <Toaster position="top-right" />
